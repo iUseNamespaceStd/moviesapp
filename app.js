19a121cb9bdef7f0e90d1fc07957379b56eb7c61
Cedric <cedricnfp@gmail.com>
2021-07-11 18:22:47 +0400

    added search filter

--- app.js
+++ app.js
@@ -1,61 +1,57 @@
+// Movie API
 const MOVIES_API = "https://gist.githubusercontent.com/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON";
-let mov = [];
-let movCopy = [];
+// Array containing the list of movies
+let movies = [];
 
-
+// Load movies on load
 window.onload = loadMovies();
 
+// Fetch api and assign the data to array
 function loadMovies() {
     fetch(MOVIES_API)
         .then(res => res.json())
         .then(data => {
-            data.forEach(movie => {
-                const card = document.createElement("div");
-                const image = document.createElement("img");
-                const info = document.createElement("div");
-
-                const wrapper = `${movie.Title} ${movie.Year}`
-                mov.push(wrapper);
-                movCopy.push(wrapper);
-
-                card.className = "card";
-
-                image.className = "movie-img";
-                image.src = movie.Images[1];
-
-                info.className = "info"
-                info.innerHTML = wrapper;
-                
-                document.querySelector('.content').append(card);
-                card.append(image);
-                card.append(info);
-            })
+            movies = JSON.parse(JSON.stringify(data));
+            buildMovies(movies);
         })
 }
 
-function filteredMovies(movies) {
-    for (let i = 0; i < movies.length; i++) {
-        const card = document.createElement("div");
-        const image = document.createElement("img");
-        const info = document.createElement("div"); 
-    
-        card.className = "card";
-        image.className = "movie-img";
-        info.className = "wrapper";
-        info.innerHTML = movies[i];
-    
-        document.querySelector('.content').append(card);
-        card.append(image);
-        card.append(info);
-
-    }
+/**
+ * Build the list of movies
+ * For Each movie a card is built and gets added to the container
+ * Finally, the container is added to the body
+ */
+function buildMovies(movies) {
+    let container = "";
+    movies.forEach(movie => {
+        container += buildMovie(movie);
+    });
+    document.querySelector('.content').innerHTML = container;
 }
 
+// Build the card for each movie
+function buildMovie(movie) {
+    // Define div element
+    let str = "<div class='card'>";
+    //Define content of div
+    if (movie && movie['Title']) {
+        if (movie['Images'] && movie['Images'].length > 0) {
+            str += ("<img class='movie-img' src='" + movie['Images'][0] + "'>"); 
+        }
+        str += ("<div>" + movie['Title'] + "</div>")
+    }
+    // Define end of div element
+    str += "</div>";
+    // Return the div element
+    return str;
+}
+
+// Filter list of movies
 function filter() {
     const userInput = document.querySelector('#search').value;
     if (userInput.length > 1) {
-        mov = movCopy.filter(m => m && m.toLowerCase().includes(userInput.toLowerCase()));
-        filteredMovies(mov);
+        const filteredMov = movies.filter(m => m && m.Title.toLowerCase().includes(userInput.toLowerCase()));
+        buildMovies(filteredMov);
     } else {
         loadMovies();
     }
